ba63c285dd8bc9f1fd4a26a57d428f97052746f6
Mads H. Danquah <mads@danquah.dk>
2016-05-19 15:38:01 +0200

    Initial project commit
    - Initial Drupal-composer setup
    - Full docker-setup
    - Full OMG setup with drupal core + one initial module (paragraphs)
    - Initial databasedump with a site-install with the standard profile
    
    SL17-36 SL17-21

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # spejderneslejr.dk
 
 This is the main website for Spejdernes Lejr 2017
+
+The project structure is based on [`drupal-composer/drupal-project`](https://github.com/drupal-composer/drupal-project).
+
+## Development dependencies
+
+* Docker and Docker Compose
+* Composer
+
+## Installation instructions
+
+1. Clone this repository
+2. From the root of the cloned repository:
+  1. Run `composer install`
+  2. Run `docker-compose build`
+  3. Run `docker-compose up`
+  4. Run `docker-compose run web drush cim -y`
+3. Profit!
+
+## Continuous Integration
+
+* We use Scrutinizer to analyze the code for code standards, debugging code and general mistakes.
+  * Codesniffer runs with Drupal standards.
+  * The ESLint file (`.eslintrc`) in the root of the project is a duplicate of Drupal 8's `.eslintrc` file. This is needed because Scrutinizer assumes that the ESLint config file is placed in the root.
---
+++ docker/docker.settings.local.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Content of example.settings.local.php.
+ */
+
+// Assertions.
+assert_options(ASSERT_ACTIVE, TRUE);
+\Drupal\Component\Assertion\Handle::register();
+
+// Enable local development services.
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+
+// Show all error messages, with backtrace information.
+$config['system.logging']['error_level'] = 'verbose';
+
+// Disable CSS and JS aggregation.
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+
+// Disable the render cache (this includes the page cache).
+# $settings['cache']['bins']['render'] = 'cache.backend.null';
+
+// Disable Dynamic Page Cache.
+# $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+
+// Allow test modules and themes to be installed.
+$settings['extension_discovery_scan_tests'] = TRUE;
+
+// Enable access to rebuild.php.
+$settings['rebuild_access'] = TRUE;
+
+/**
+ * Site specific development settings.
+ */
