b8e3d07db8e00bdd92d84152cf5ba7f813aa862a
Thomas Slott <bigslott@gmail.com>
2016-05-23 12:00:33 +0200

    Init new base theme - "Camp"

---
+++ web/themes/custom/camp/gulpfile.js
@@ -0,0 +1,53 @@
+// Get modules.
+var
+  gulp = require('gulp'),
+  notify = require('gulp-notify'),
+  sass = require('gulp-sass'),
+  jshint = require('gulp-jshint'),
+  stylish = require('jshint-stylish'),
+  autoprefixer = require('gulp-autoprefixer'),
+  sourcemaps = require('gulp-sourcemaps');
+
+// Sass.
+gulp.task('sass', function () {
+  return gulp.src('./sass/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: require('node-neat').includePaths,
+      outputStyle: 'expanded'
+    }).on('error', notify.onError(function (error) {
+      return 'SASS error: ' + error.message;
+    })))
+    .pipe(autoprefixer({
+      browsers: ['last 4 versions'],
+      cascade: false
+    }))
+    .pipe(sourcemaps.write('sourcemaps'))
+    .pipe(gulp.dest('./css'));
+});
+
+// Sass watch.
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+// JsHint.
+gulp.task('jshint', function() {
+  return gulp.src(['gulfile.js', './js/*.js'])
+    .pipe(jshint())
+    // Get stylish output.
+    .pipe(jshint.reporter(stylish))
+    // Add fail reporter and send it to notification API.
+    .pipe(jshint.reporter('fail'))
+    .on('error', notify.onError(function (error) {
+          return 'JSHint error: ' + error.message;
+    }));
+});
+
+// JsHint watch.
+gulp.task('jshint:watch', function () {
+  gulp.watch(['gulpfile.js', './js/*.js'], ['jshint']);
+});
+
+// Register workers.
+gulp.task('default', ['jshint', 'sass', 'jshint:watch', 'sass:watch']);
