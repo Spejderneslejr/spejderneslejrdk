143d68c80f2fd705b8b09c26273d559dbaca105e
Mads H. Danquah <mads@danquah.dk>
2016-05-26 22:36:50 +0200

    Set permissions and do updb after cmi during reset
    SL17-36

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -7,7 +7,11 @@ cd "${DIR}/../"
 # Chmod to 777 if the file is not owned by www-data
 find web/sites/default/files \! -uid 33  \! -name .gitkeep | xargs chmod 777
 
+# Make sites/default read-only and executable
+chmod 555 web/sites/default
+
 time docker-compose run --entrypoint "sh -c" --rm drush " \
-  drush updb -y && \
   drush cim -y && \
-  drush --uri=http://local.docker uli"
+  drush updb -y && \
+  drush --uri=http://local.docker uli && \
+  drush cr"
