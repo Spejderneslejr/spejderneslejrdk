d02ba1e047d18097558ceef2c210e2e7d6448f4c
Mads H. Danquah <mads@danquah.dk>
2016-06-13 08:54:10 +0200

    Requiring sudo when resettings file-permissions

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -5,10 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "${DIR}/../"
 # Chmod to 777 if the file is not owned by www-data
-find web/sites/default/files \! -uid 33  \! -name .gitkeep | xargs chmod 777
+find web/sites/default/files \! -uid 33  \! -name .gitkeep | sudo xargs chmod 777
 
 # Make sites/default read-only and executable
-chmod 555 web/sites/default
+sudo chmod 555 web/sites/default
 
 time docker-compose run --entrypoint "sh -c" --rm drush " \
   drush cim -y && \
