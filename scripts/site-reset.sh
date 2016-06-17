48b2371bd70566caf4f0012b25a614ced164e329
Mads H. Danquah <mads@danquah.dk>
2016-06-17 11:08:49 +0200

    Updating reset-scripts

--- scripts/site-reset.sh
+++ scripts/site-reset.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 IFS=$'\n\t'
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd "${DIR}/../"
+cd "${SCRIPT_DIR}/../"
 # Chmod to 777 if the file is not owned by www-data
 find web/sites/default/files \! -uid 33  \! -name .gitkeep | sudo xargs chmod 777
 
@@ -13,5 +13,5 @@ sudo chmod 555 web/sites/default
 time docker-compose run --entrypoint "sh -c" --rm drush " \
   drush cim -y && \
   drush updb -y && \
-  drush --uri=http://local.docker uli && \
-  drush cr"
+  drush cr
+  "
