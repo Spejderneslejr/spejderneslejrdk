36734f2f874672a3e9e297fdf5587df0ef1598e6
Mads H. Danquah <mads@danquah.dk>
2016-06-21 15:11:25 +0200

    Make the reset-script support spaces in filenames

--- scripts/site-reset.sh
+++ scripts/site-reset.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "${SCRIPT_DIR}/../"
 # Chmod to 777 if the file is not owned by www-data
-find web/sites/default/files \! -uid 33  \! -name .gitkeep | sudo xargs chmod 777
+find web/sites/default/files \! -uid 33  \! -print0 -name .gitkeep | sudo xargs -0 chmod 777
 
 # Make sites/default read-only and executable
 sudo chmod 555 web/sites/default
