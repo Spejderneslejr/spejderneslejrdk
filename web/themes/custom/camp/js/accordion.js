251711b51ec99e32af16e83a257196d147186ceb
Thomas Slott <bigslott@gmail.com>
2016-06-23 12:06:29 +0200

    Code style cleanup
    Sl17-69

--- web/themes/custom/camp/js/accordion.js
+++ web/themes/custom/camp/js/accordion.js
@@ -6,10 +6,10 @@
   Drupal.behaviors.accordion = {
     attach: function (context, settings) {
 
+      // Enable toggle of the accoridon content of each type of
+      // accordion.
       $('.accordion').each(function() {
-
         var element = $(this);
-
         element.find('.accordion__header', context).click(function() {
           $(this).toggleClass('active');
           element.find('.accordion__content').slideToggle();
