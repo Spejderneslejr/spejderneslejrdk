7768601bbfc647d434e3a48fe21e61d454628edb
Mads H. Danquah <mads@danquah.dk>
2016-07-12 11:21:02 +0200

    Updating docker-reset to only ask for sudo creds once

--- scripts/docker-reset.sh
+++ scripts/docker-reset.sh
@@ -7,18 +7,31 @@ echoc () {
 	echo -e "\e[38;5;34m$1\e[0m"
 }
 
-# Clear all running containers.
+# Preemptive sudo lease - to let you go out and grab a coffee while the script
+# runs.
 sudo echo ""
+
+# Clear all running containers.
 echoc "*** Removing existing containers" 
 docker-compose kill && docker-compose rm --all -v -f
+
+# Composer silently kills any valid sudo leases, to avoid elevation-exploits in
+# scripts - we disable this to make sure we only have to give sudo a password
+# once.
 echoc "Composer install'ing"
-composer install
+COMPOSER_ALLOW_SUPERUSER=1 composer install
+
 # Start up containers in the background and continue imidiately
 echoc "*** Starting new containers" 
-docker-compose up --remove-orphans -d 
+docker-compose up --remove-orphans -d
+
 # Sleep while containers are starting up then perform a reset
 sleep 15 
-echoc "*** Resetting Drupal" && "${SCRIPT_DIR}/site-reset.sh"
+
+# Perform the drupal-specific reset
+echoc "*** Resetting Drupal"
+"${SCRIPT_DIR}/site-reset.sh"
+
 # Done, bring the background docker-compose logs back into foreground
 echoc "*** Done, watching logs"
 docker-compose logs -f
