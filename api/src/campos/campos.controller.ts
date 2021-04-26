1ff96564dd5ba65a282efd0e274def3fee782805
Jacob Pihl <jacobengb@gmail.com>
2021-04-26 20:45:12 +0200

    Added more Odoo fields to the campos controller

--- api/src/campos/campos.controller.ts
+++ api/src/campos/campos.controller.ts
@@ -8,7 +8,7 @@ export class CamposController {
   @Get('jobs')
   async jobs(): Promise<string> {
     return await this.camposService.executeKw('hr.job', 'search_read', [], {
-      fields: ['name', 'organization_id'],
+      fields: ['name', 'organization_id','state','user_id','description','description_time_and_scope','description_we_give','description_you_give','write_date'],
       context: { show_org_path: true },
     });
   }
