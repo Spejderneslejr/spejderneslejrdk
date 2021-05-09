be46208a6edf3080146e1dc2f2fc979cb4c451b7
Jacob Pihl <jacobengb@gmail.com>
2021-05-09 23:17:35 +0200

    added About SL vue component

---
+++ drupal/apps/jobs/src/components/AboutSL.vue
@@ -0,0 +1,17 @@
+<template>
+<span class="about-sl">
+Spejdernes Lejr er et unikt tværkorpsligt samarbejde blandt danske spejderorganisationer om at afholde Nordens største spejderlejr med 40.000 deltagende spejdere og
+ et tilsvarende antal gæster. Primære målgruppe for deltagerne er børn og unge i alderen 6 – 23 år.<br><br>
+ Lejren bliver afholdt i Hedeland Naturpark ved Høje Taastrup fra den 23. juli til den 31. juli 2022 under temaet ’Fælles om fremtiden’. 
+ Vi håber, at du vil være en del af holdet. Lejren er drevet af frivillige og kan kun afholdes takket være tusindvis af frivilliges indsats for at give deltagerne
+  en fantastisk spejderoplevelse.<br><br>
+  Læs mere om at være frivillig i Spejdernes Lejr 2022 <b><a href="https://spejderneslejr.dk/da/job">her</a></b>
+</span>
+</template>
+
+<script>
+export default {
+  props: {},
+  methods: {},
+};
+</script>
