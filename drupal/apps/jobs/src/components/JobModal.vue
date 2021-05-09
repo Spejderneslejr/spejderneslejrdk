d88b76b470ae96b23f5ecccf0db43af008810071
Jacob Pihl <jacobengb@gmail.com>
2021-05-09 23:17:53 +0200

    updated modal with new fields

--- drupal/apps/jobs/src/components/JobModal.vue
+++ drupal/apps/jobs/src/components/JobModal.vue
@@ -3,8 +3,9 @@
     <div class="modal">
       <header class="modal-header">
         <slot name="header">
-          <h4><i class="far fa-compass"></i><span style="margin-left:5px">Område</span></h4>
-          <h2 style="margin:0;">{{job.name}}</h2>
+          <h4><JobIcon :area="job.area" /><span style="margin-left:5px">{{job.area}}</span></h4>
+          <h2 class="header">{{job.name}}</h2>
+          <p class="teaser">{{job.teaser}}</p>
           </slot>
         <button type="button" class="btn-close" @click="close"><i class="fas fa-times fa-lg"></i></button>
       </header>
@@ -29,13 +30,14 @@
           </div>
           <div class="content">
           <p class="title">Om Spejdernes Lejr</p>
-          bla bla
+          <AboutSL />
           </div>
           </slot>
       </section>
       <hr>
       <footer class="modal-footer">
         <slot name="footer"></slot>
+        <span class="date">Oprettet {{job.formatted_create_date}}</span>
         <button type="button" class="right" @click="close">
           LUK
         </button>
@@ -45,7 +47,14 @@
 </template>
 
 <script>
+import JobIcon from "./JobIcon.vue";
+import AboutSL from "./AboutSL.vue";
+
 export default {
+      components: {
+      JobIcon,
+      AboutSL,
+  },
   name: "Modal",
   methods: {
     close() {
