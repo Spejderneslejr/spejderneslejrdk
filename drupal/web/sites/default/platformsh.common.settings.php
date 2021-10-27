3d590ad345344657b47ff66fc9bd80c6c728741b
Mads H. Danquah <mads@danquah.dk>
2021-10-27 22:13:07 +0200

    D8 -> D9 upgrade

--- drupal/web/sites/default/platformsh.common.settings.php
+++ drupal/web/sites/default/platformsh.common.settings.php
@@ -1,4 +1,7 @@
 <?php
+
+use Drupal\Core\Installer\InstallerKernel;
+
 /**
  * @file
  * Platform.sh settings shared between environments.
@@ -58,7 +61,7 @@ if (isset($_ENV['PLATFORM_RELATIONSHIPS'])) {
 // Redis configuration.
 if (!empty($_ENV['PLATFORM_RELATIONSHIPS'])) {
     $relationships = json_decode(base64_decode($_ENV['PLATFORM_RELATIONSHIPS']), true);
-    if (!empty($relationships['redis'][0]) && !drupal_installation_attempted() && extension_loaded('redis')) {
+    if (!empty($relationships['redis'][0]) && !InstallerKernel::installationAttempted() && extension_loaded('redis')) {
         $redis = $relationships['redis'][0];
         // Set Redis as the default backend for any cache bin not otherwise
         // specified.
@@ -146,67 +149,3 @@ $settings['hash_salt'] = '9e62c5b69893a3d2816dced2cf45d4b4c0467dc1b451db87e717ea
 // platformsh.env.<env>.settings.php files if necessary (read: prod).
 // test.dds.dk google analytics property.
 $config['google_analytics.settings']['account'] = 'UA-7162673-25';
-
-// Redis configuration.
-if (!empty($_ENV['PLATFORM_RELATIONSHIPS'])) {
-    $relationships = json_decode(base64_decode($_ENV['PLATFORM_RELATIONSHIPS']), true);
-
-    if (!empty($relationships['redis'][0]) && !drupal_installation_attempted() && extension_loaded('redis')) {
-        $redis = $relationships['redis'][0];
-
-        // Set Redis as the default backend for any cache bin not otherwise
-        // specified.
-        $settings['cache']['default'] = 'cache.backend.redis';
-        $settings['redis.connection']['host'] = $redis['host'];
-        $settings['redis.connection']['port'] = $redis['port'];
-
-        // Apply changes to the container configuration to better leverage Redis.
-        // This includes using Redis for the lock and flood control systems, as well
-        // as the cache tag checksum. Alternatively, copy the contents of that file
-        // to your project-specific services.yml file, modify as appropriate, and
-        // remove this line.
-        $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
-
-        // Allow the services to work before the Redis module itself is enabled.
-        $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
-
-        // Manually add the classloader path, this is required for the container
-        // cache bin definition below and allows to use it without the redis module
-        // being enabled.
-        $class_loader->addPsr4('Drupal\\redis\\', 'modules/contrib/redis/src');
-
-        // Use redis for container cache.
-        // The container cache is used to load the container definition itself, and
-        // thus any configuration stored in the container itself is not available
-        // yet. These lines force the container cache to use Redis rather than the
-        // default SQL cache.
-        $settings['bootstrap_container_definition'] = [
-            'parameters' => [],
-            'services' => [
-                'redis.factory' => [
-                    'class' => 'Drupal\redis\ClientFactory',
-                ],
-                'cache.backend.redis' => [
-                    'class' => 'Drupal\redis\Cache\CacheBackendFactory',
-                    'arguments' => [
-                        '@redis.factory',
-                        '@cache_tags_provider.container',
-                        '@serialization.phpserialize',
-                    ],
-                ],
-                'cache.container' => [
-                    'class' => '\Drupal\redis\Cache\PhpRedis',
-                    'factory' => ['@cache.backend.redis', 'get'],
-                    'arguments' => ['container'],
-                ],
-                'cache_tags_provider.container' => [
-                    'class' => 'Drupal\redis\Cache\RedisCacheTagsChecksum',
-                    'arguments' => ['@redis.factory'],
-                ],
-                'serialization.phpserialize' => [
-                    'class' => 'Drupal\Component\Serialization\PhpSerialize',
-                ],
-            ],
-        ];
-    }
-}
