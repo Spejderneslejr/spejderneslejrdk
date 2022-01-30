e1139c28358f180257f17f5fdc38d206600b3ed7
Mads H. Danquah <mads@danquah.dk>
2022-01-30 22:40:22 +0100

    Specify langcode when fetching jobs

--- api/src/campos/campos.controller.ts
+++ api/src/campos/campos.controller.ts
@@ -26,7 +26,7 @@ export class CamposController {
     const jobs: Array<Job> = (await this.camposService.executeKw('hr.job', 'search_read', [], {
       fields: ['name', 'organization_id', 'area_organization_id', 'state', 'user_id', 'teaser', 'description', 'description_time_and_scope', 'description_we_give',
         'description_you_give', 'no_of_recruitment', 'no_of_hired_employee', 'write_date', 'create_date', 'user_name', 'user_email'],
-      context: { show_org_path: true },
+      context: { show_org_path: true, "lang": "da_DK" },
     }) as unknown) as Array<Job>;
 
     const cleanedJobs = jobs.map((job) => {
