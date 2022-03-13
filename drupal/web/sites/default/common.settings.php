d5a53d1d7dd7c9bde9cbfb91a86e9aff69f40f7c
Mads H. Danquah <mads@danquah.dk>
2022-03-13 21:30:48 +0100

    Add twig_xdebug for better twig debugging.

--- drupal/web/sites/default/common.settings.php
+++ drupal/web/sites/default/common.settings.php
@@ -5,3 +5,5 @@ $settings['config_sync_directory'] = __DIR__ . '/../../../config/drupal_cmi_sync
 // Disable the production tag-manager container in all environment and let
 // master enable it itself via platformsh.env.master.settings.php.
 $config['google_tag.container.production']['status'] = false;
+
+$settings['config_exclude_modules'] = ['twig_xdebug'];
