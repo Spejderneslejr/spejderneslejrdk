65fcdc26986466161d2dd76f7a4a71fcc9a0b281
Mads Høgstedt Danquah <mads@reload.dk>
2022-05-15 19:38:46 +0200

    Add IST job link

--- drupal/apps/jobs/src/App.vue
+++ drupal/apps/jobs/src/App.vue
@@ -12,6 +12,7 @@
     <ul>
       <li>Har du allerede en aftale om et konkret job - så brug <a href="https://tilmelding.spejderneslejr.dk/member/job/110">dette link</a></li>
       <li>Har du ikke fundet et konkret job, men gerne vil være lejrfrivillig - så brug <a href="https://tilmelding.spejderneslejr.dk/member/job/111">dette link</a> i stedet</li>
+      <li>Sign-up for IST - International Service Team - <a href="https://tilmelding.spejderneslejr.dk/en/member/job/257">use this link</a>.</li>
     </ul>
     </div>
     <!-- <DepartmentSelector /> -->
