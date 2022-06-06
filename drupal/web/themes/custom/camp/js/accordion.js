5cc202a22dc5eb73546a2472b3732563c2bcf8e2
Mads H. Danquah <mads@danquah.dk>
2022-06-06 22:54:35 +0200

    Initial work on an anchor feature

--- drupal/web/themes/custom/camp/js/accordion.js
+++ drupal/web/themes/custom/camp/js/accordion.js
@@ -6,10 +6,21 @@
   Drupal.behaviors.accordion = {
     attach: function (context, settings) {
 
-      // Enable toggle of the accoridon content of each type of
+      // Enable toggle of the accordion content of each type of
       // accordion.
       $('.accordion').each(function() {
         var element = $(this);
+        var header = element.find('.anchor', context);
+
+        // Attempt to open accordion if a deeplink is detected.
+        if (header) {
+          var id = header.attr('name');
+          if (id && window.location.hash == "#" + id) {
+            $(this).toggleClass('active');
+            element.find('.accordion__content').slideToggle();
+          }
+        }
+
         element.find('.accordion__header', context).click(function() {
           $(this).toggleClass('active');
           element.find('.accordion__content').slideToggle();
