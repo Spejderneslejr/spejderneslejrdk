2cc228cbb72b24813f0a4a56713638d6df3b6964
Mads H. Danquah <mads@danquah.dk>
2022-07-22 18:50:40 +0200

    Futher tune mastecam

--- drupal/scripts/fetch-mastecam.sh
+++ drupal/scripts/fetch-mastecam.sh
@@ -5,20 +5,39 @@ set -euxo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cams="cam1.jpg cam2.jpg cam3.jpg cam4.jpg"
+camDestDir="${SCRIPT_DIR}/../web/sites/default/files/mastecam"
 
 # Iterate the string variable using for loop
-mkdir -p "${SCRIPT_DIR}/web/sites/default/files/mastecam"
+mkdir -p "${camDestDir}"
+
+function do_convert(){
+  local dir=${1}
+  local file=${2}
+  local geo=${3}
+
+  convert \
+      -sampling-factor 4:2:0 \
+      -strip \
+      -quality 80 \
+      -interlace Plane \
+      -sharpen 0.2 \
+      -colorspace sRGB \
+      -geometry "${geo}>" \
+      "${dir}/${file}" \
+      "${dir}/${geo}-${file}"
+}
+
 set +e
 IFS=' '
 for cam in $cams; do
   if wget -O "/tmp/${cam}" "https://foto.sl2022.dk/cam/${cam}"; then
-    mv "/tmp/${cam}" "${SCRIPT_DIR}/web/sites/default/files/mastecam"
-    convert -verbose -quality 70 -geometry "1200x1200>" \
-      "${SCRIPT_DIR}/web/sites/default/files/mastecam/${cam}" \
-      "${SCRIPT_DIR}/web/sites/default/files/mastecam/1200-${cam}"
+    mv "/tmp/${cam}" "${camDestDir}/"
+    do_convert "${camDestDir}" "${cam}" "325x325"
+    do_convert "${camDestDir}" "${cam}" "650x650"
+    do_convert "${camDestDir}" "${cam}" "1300x1300"
+    do_convert "${camDestDir}" "${cam}" "2600x2600"
   fi
 done
 set -e
-
 
 
